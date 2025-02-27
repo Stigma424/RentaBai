9c7f675cedb19dc4ccccc12db10ab6ed1b07b74b
Stigma424 <125037090+Stigma424@users.noreply.github.com>
2025-02-27 20:13:44 +0800

    Test

---
+++ app/src/main/java/com/example/rentabai/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.rentabai
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
