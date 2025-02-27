07e78afb0b1df1f0ca27b810e228767577ea6e74
Stigma424 <125037090+Stigma424@users.noreply.github.com>
2025-02-27 23:09:58 +0800

    Logic And Validation Backend for Developer Page and Login Page

--- app/src/main/java/com/example/rentabai/DeveloperActivity.kt
+++ app/src/main/java/com/example/rentabai/DeveloperActivity.kt
@@ -3,7 +3,11 @@ package com.example.rentabai
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,11 +18,38 @@ class DeveloperActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.developer_page)
 
+        var devNumber = 1;
         val backButton = findViewById<Button>(R.id.BackButton)
-
+        val shiftButton = findViewById<Button>(R.id.ShiftButton)
+        val devPfp = findViewById<ImageView>(R.id.DevPfp)
+        val devName = findViewById<TextView>(R.id.DevName)
+        val devBio = findViewById<TextView>(R.id.DevBio)
         backButton.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
+
+        shiftButton.setOnClickListener {
+            if(devNumber == 1){
+                devPfp.setImageResource(R.drawable.test2)
+                devName.text = "Gabriel Montil"
+                devBio.text = "Ambot oi Bio"
+                shiftButton.text = "Stigma424"
+                devNumber = 2
+                Log.e("Notification","Inside Dev 1 ")
+            }
+            else if(devNumber == 2){
+                devPfp.setImageResource(R.drawable.test1)
+                devName.text = "John Dhenzell Ermac"
+                devBio.text = "Basta Bios"
+                shiftButton.text = "BonelessGod44"
+                devNumber = 1
+                Log.e("Notification","Inside Dev 2")
+            }
+            else{
+                Toast.makeText(this,"Something Wrong", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
 }
--- app/src/main/java/com/example/rentabai/LoginActivity.kt
+++ app/src/main/java/com/example/rentabai/LoginActivity.kt
@@ -16,17 +16,39 @@ class LoginActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
 
+
+        val tempUser = "User"
+        val tempPass = "123"
         val usernameEditText = findViewById<EditText>(R.id.UsernameTextBox)
         val passwordEditText = findViewById<EditText>(R.id.PasswordTextBox)
         val loginButton = findViewById<Button>(R.id.LogInButton)
 
         loginButton.setOnClickListener {
-            val intent = Intent(this, LandingPageActivity::class.java)
-            startActivity(intent)
+
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if(isNullOrEmpty(username,password)){
+                if(isValid(username,password,tempUser,tempPass)){
+                    val intent = Intent(this, LandingPageActivity::class.java)
+                    startActivity(intent)
+
+                }
+                else{
+                    Toast.makeText(this,"Incorrect Username Or Password",Toast.LENGTH_LONG).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"Please Input Required Fields",Toast.LENGTH_LONG).show()
+            }
+
         }
     }
     //Method for Checking Username and Password Validation
     private fun isValid(inputUsername: String, inputPassword: String,username:String,password:String): Boolean {
-        return (inputUsername == username && password == inputPassword);
+        return inputUsername == username && password == inputPassword
+    }
+    private fun isNullOrEmpty(inputUsername: String,inputPassword: String):Boolean{
+        return !(inputPassword.isNullOrEmpty() || inputUsername.isNullOrEmpty())
     }
 }
