672c957862af987373d9891ebf755514ea6bc7e5
bonelessGod44 <136452558+bonelessGod44@users.noreply.github.com>
2025-02-28 21:45:40 +0800

    Added bottom navigation bar

--- app/src/main/java/com/example/rentabai/LandingActivity.kt
+++ app/src/main/java/com/example/rentabai/LandingActivity.kt
@@ -1,12 +1,46 @@
 package com.example.rentabai
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class LandingActivity : Activity() {
+class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val imageAccount: ImageView = findViewById(R.id.account_button) // Find the ImageView
+
+        val firstFragment = HomeFragment()
+        val secondFragment = SettingsFragment()
+        val thirdFragment = DevProfileFragment()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> setCurrentFragment(firstFragment)
+                R.id.nav_settings -> setCurrentFragment(secondFragment)
+                R.id.nav_logout -> setCurrentFragment(thirdFragment)
+            }
+            true
+        }
+
+
+        imageAccount.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.bottomNavigationView, fragment)
+            commit()
+        }
     }
 }
